cd01dc1929b5d56413d0a5dcd708772fa27e0d3c
maxxxsudb <77297275+maxxxsudb@users.noreply.github.com>
2026-02-06 18:55:54 +0300

    Refactor log processing in test.sh

--- test.sh
+++ test.sh
@@ -1,35 +1,24 @@
 kubectl run netcat-test --image=busybox -it --rm -- /bin/sh -c "echo '<165>Feb 06 13:44:35 TEST_HOST My_test_message_123' | nc -u -w1 syslog-service 514"
-otelcol.processor.transform "fix_logs" {
-  error_mode = "ignore"
-
-  // БЛОК 1: ЗАПОЛНЯЕМ ШАПКУ (Resource Context)
-  // Это уберет прочерки в начале строки. Экспортер смотрит только сюда!
-  log_statements {
-    context = "resource"
-    statements = [
-      // Жестко задаем хост и имя программы, чтобы исключить ошибки переменных
-      "set(attributes[\"host.name\"], \"k8s-fixed-node\")",
-      "set(attributes[\"service.name\"], \"my-fixed-app\")",
-    ]
-  }
-
-  // БЛОК 2: ЗАПОЛНЯЕМ ТЕЛО И ID (Log Context)
-  log_statements {
+log_statements {
     context = "log"
     statements = [
-      // ProcID - это свойство лога
-      "set(attributes[\"proc_id\"], \"1\")",
+      // 1. ЧИНИМ ХОСТ (HOSTNAME)
+      // Берем из ресурса (k8s.pod.name) и кладем в атрибут "hostname" (без точки!)
+      // Если пода нет, ставим заглушку.
+      "set(attributes[\"hostname\"], resource.attributes[\"k8s.pod.name\"])",
+      "set(attributes[\"hostname\"], \"manual-host\") where attributes[\"hostname\"] == nil",
 
-      // ГЛАВНОЕ: Если Body вдруг пустое, пишем туда текст
-      // В твоем логе (скрин 3) текст уже есть, но на всякий случай:
-      "set(body, \"DEBUG: MESSAGE BODY WAS NIL\") where body == nil",
+      // 2. ЧИНИМ ИМЯ ПРИЛОЖЕНИЯ (APP-NAME)
+      // Берем имя контейнера и кладем в атрибут "appname" (без дефиса!)
+      "set(attributes[\"appname\"], resource.attributes[\"k8s.container.name\"])",
+      "set(attributes[\"appname\"], \"manual-app\") where attributes[\"appname\"] == nil",
+
+      // 3. ЧИНИМ ТЕЛО СООБЩЕНИЯ (MSG)
+      // Экспортер игнорирует Body. Ему нужно, чтобы текст лежал в attributes["message"].
+      // Копируем Body туда.
+      "set(attributes[\"message\"], body)",
+      
+      // На всякий случай, если Body пришел сложным объектом (JSON), превращаем в строку
+      "set(attributes[\"message\"], Concat([attributes[\"message\"]], \"\"))",
     ]
   }
-
-  output {
-    logs = [
-        otelcol.exporter.syslog.arcsight_debug.input,
-        otelcol.exporter.debug.inspector.input, // Чтобы ты видел в UI результат
-    ]
-  }
-}
